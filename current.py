f7e10d210687da10d4bcf74178f43f66e35e1b6b
kushal <kushaldeveloper@gmail.com>
2020-08-11 18:40:39 -0600

    format code using black

--- current.py
+++ current.py
@@ -16,11 +16,7 @@ for story in top_stories:
         f"https://hacker-news.firebaseio.com/v0/item/{story}.json",
         headers={"Cache-Control": "no-cache", "Pragma": "no-cache"},
     ).json()
-    if (
-        "title" in story_data
-        and "score" in story_data
-        and "url" in story_data
-    ):
+    if "title" in story_data and "score" in story_data and "url" in story_data:
         print(f"\nStory: {story}")
         title = story_data.get("title")
         score = story_data["score"]
@@ -32,20 +28,14 @@ for story in top_stories:
     if "kids" in story_data:
         kids = story_data.get("kids")
         for kid in kids:
-            comment_url = (
-                f"https://hacker-news.firebaseio.com/v0/item/{kid}.json"
-            )
+            comment_url = f"https://hacker-news.firebaseio.com/v0/item/{kid}.json"
             print(comment_url)
             comment_data = requests.get(
                 comment_url,
-                headers={
-                    "Cache-Control": "no-cache",
-                    "Pragma": "no-cache",
-                },
+                headers={"Cache-Control": "no-cache", "Pragma": "no-cache",},
             ).json()
             if comment_data and (
-                "deleted" not in comment_data
-                or comment_data["deleted"] != "True"
+                "deleted" not in comment_data or comment_data["deleted"] != "True"
             ):
                 if "by" in comment_data:
                     comment_by = comment_data.get("by")
@@ -57,7 +47,9 @@ for story in top_stories:
                 if "kids" in comment_data:
                     replies = comment_data.get("kids")
                     for kid in kids:
-                        reply_url = f"https://hacker-news.firebaseio.com/v0/item/{kid}.json"
+                        reply_url = (
+                            f"https://hacker-news.firebaseio.com/v0/item/{kid}.json"
+                        )
                         print(reply_url)
                         reply_data = requests.get(
                             reply_url,
