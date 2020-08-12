96754be687ed6e73ea49cb1836d7c3b09848280b
kushal <kushaldeveloper@gmail.com>
2020-08-11 18:46:12 -0600

    remove all code for kids

--- current.py
+++ current.py
@@ -31,38 +31,4 @@ for story in top_stories:
                 comment_url,
                 headers={"Cache-Control": "no-cache", "Pragma": "no-cache",},
             ).json()
-            if comment_data and (
-                "deleted" not in comment_data or comment_data["deleted"] != "True"
-            ):
-                if "by" in comment_data:
-                    comment_by = comment_data.get("by")
-                if "text" in comment_data:
-                    comment_text = comment_data.get("text")
-                    continue
-                if "time" in comment_data:
-                    comment_time = comment_data.get("time")
-                if "kids" in comment_data:
-                    replies = comment_data.get("kids")
-                    for kid in kids:
-                        reply_url = (
-                            f"https://hacker-news.firebaseio.com/v0/item/{kid}.json"
-                        )
-                        print(reply_url)
-                        reply_data = requests.get(
-                            reply_url,
-                            headers={
-                                "Cache-Control": "no-cache",
-                                "Pragma": "no-cache",
-                            },
-                        ).json()
-                        if reply_data and (
-                            "deleted" not in reply_data
-                            or reply_data["deleted"] != "True"
-                        ):
-                            if "by" in reply_data:
-                                reply_by = reply_data.get("by")
-                            if "text" in reply_data:
-                                reply_text = reply_data.get("text")
-                                continue
-                            if "time" in reply_data:
-                                reply_time = reply_data.get("time")
+            print(comment_data)
